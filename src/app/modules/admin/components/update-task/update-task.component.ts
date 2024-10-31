abba55147e7b2c9a94717258d353116d2cf75a49
Dota43ver <facundoagueroc@gmail.com>
2024-10-31 16:05:34 -0300

    update task

--- src/app/modules/admin/components/update-task/update-task.component.ts
+++ src/app/modules/admin/components/update-task/update-task.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update-task',
@@ -9,14 +11,66 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UpdateTaskComponent {
   id: number = this.route.snapshot.params['id'];
+  updateTaskForm!: FormGroup;
+  statuses: string[] = [
+    'PENDING',
+    'INPROGRESS',
+    'COMPLETED',
+    'DEFERRED',
+    'CANCELLED',
+  ];
+  listOfEmployees: any = [];
+  listOfPriorities: any = ['LOW', 'MEDIUM', 'HIGH'];
 
-  constructor(private service: AdminService, private route: ActivatedRoute) {
+  constructor(
+    private service: AdminService,
+    private route: ActivatedRoute,
+    private fb: FormBuilder,
+    private adminService: AdminService,
+    private snackBar: MatSnackBar,
+    private router: Router
+  ) {
     this.getTaskById();
+    this.getUsers();
+    this.updateTaskForm = this.fb.group({
+      employeeId: [null, [Validators.required]],
+      priority: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+      dueDate: [null, [Validators.required]],
+      taskStatus: [null, [Validators.required]],
+    });
   }
 
   getTaskById() {
     this.service.getTaskById(this.id).subscribe((res) => {
+      this.updateTaskForm.patchValue(res);
       console.log(res);
     });
+  }
+
+  getUsers() {
+    this.adminService.getUsers().subscribe((res) => {
+      this.listOfEmployees = res;
+      console.log(res);
+    });
+  }
+
+  updateTask() {
+    this.adminService
+      .updateTask(this.id, this.updateTaskForm.value)
+      .subscribe((res) => {
+        if (res.id != null) {
+          this.snackBar.open('Task updated successfully', 'Close', {
+            duration: 2000,
+          });
+          this.router.navigateByUrl('/admin/dashboard');
+        } else {
+          this.snackBar.open('Something went wrong', 'ERROR', {
+            duration: 2000,
+          });
+        }
+      });
+    console.log('esto es el value del form', this.updateTaskForm.value);
   }
 }
