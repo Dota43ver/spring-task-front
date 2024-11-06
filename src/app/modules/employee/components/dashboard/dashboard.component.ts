3dd3cd4af149a9dfc57b3fd3324962264d99c1f0
Dota43ver <facundoagueroc@gmail.com>
2024-11-05 21:18:49 -0300

    employee taks cards in dashboard

--- src/app/modules/employee/components/dashboard/dashboard.component.ts
+++ src/app/modules/employee/components/dashboard/dashboard.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
+  listOfTasks: any = [];
 
+  constructor(private service: EmployeeService) {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.service.getEmployeeTasksById().subscribe((res) => {
+      console.log(res);
+      this.listOfTasks = res;
+    });
+  }
 }
--- src/app/modules/employee/employee.module.ts
+++ src/app/modules/employee/employee.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { EmployeeRoutingModule } from './employee-routing.module';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DemoAngularMaterialModule } from 'src/app/DemoAngularMaterialModule';
 
 @NgModule({
-  declarations: [
-    DashboardComponent
-  ],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    EmployeeRoutingModule
-  ]
+    EmployeeRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DemoAngularMaterialModule,
+  ],
 })
-export class EmployeeModule { }
+export class EmployeeModule {}
