3a31791dd0a6b60ecaceeae0c4e4599f688615c5
Dota43ver <facundoagueroc@gmail.com>
2024-11-11 18:21:51 -0300

    task status change by the employee

--- src/app/modules/employee/components/dashboard/dashboard.component.ts
+++ src/app/modules/employee/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { EmployeeService } from '../../services/employee.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,13 +10,28 @@ import { EmployeeService } from '../../services/employee.service';
 export class DashboardComponent {
   listOfTasks: any = [];
 
-  constructor(private service: EmployeeService) {
+  constructor(private service: EmployeeService, private snackbar: MatSnackBar) {
     this.getTasks();
   }
 
   getTasks() {
     this.service.getEmployeeTasksById().subscribe((res) => {
       this.listOfTasks = res;
+    });
+  }
+
+  updateStatus(id: number, status: string) {
+    this.service.updateStatus(id, status).subscribe((res) => {
+      if (res.id != null) {
+        this.snackbar.open('Task status updated successfully', 'Close', {
+          duration: 3000,
+        });
+        this.getTasks();
+      } else {
+        this.snackbar.open('Getting error updating task', 'Close', {
+          duration: 3000,
+        });
+      }
     });
   }
 }
--- src/app/modules/employee/services/employee.service.ts
+++ src/app/modules/employee/services/employee.service.ts
@@ -17,6 +17,12 @@ export class EmployeeService {
     });
   }
 
+  updateStatus(id: number, status: string): Observable<any> {
+    return this.http.get(BASIC_URL + `api/employee/task/${id}/${status}`, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
   private createAuthorizationHeader(): HttpHeaders {
     return new HttpHeaders().set(
       'Authorization',
